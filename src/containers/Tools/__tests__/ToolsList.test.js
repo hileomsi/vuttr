9cdbfc48111b43362357dc109b7202d1ee9f123d
Hiléo Andersson <hileomsi@gmail.com>
2019-11-08 09:30:34 -0300

    remove test containers for tests CI

--- src/containers/Tools/__tests__/ToolsList.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import ToolsList from '../ToolsList';
-
-describe('Testing Header Component', () => {
-  it('renders without crashing', () => {
-    shallow(<ToolsList />);
-  });
-});
